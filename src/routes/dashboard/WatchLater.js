570baae4924b5fb653a02ddad20b939e05bb41a8
Nomad-Rob <rfarley89@gmail.com>
2024-07-06 18:16:43 -0500

    Task 8 ?

---
+++ src/routes/dashboard/WatchLater.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WatchLater = () => {
+  return (
+    <div>
+      <h2>Watch Later</h2>
+      <p>Here are the movies you want to watch later!</p>
+    </div>
+  );
+};
+
+export default WatchLater;
